a1253d7e228322862b64dd3d98b5ff21d6a0cf1b
Kasperi Kiviluoma <k.kiviluoma@pm.me>
2023-10-15 20:02:12 +0300

    sq

--- app/src/main/java/com/example/ajoittaja/MainActivity.kt
+++ app/src/main/java/com/example/ajoittaja/MainActivity.kt
@@ -1,58 +1,39 @@
 package com.example.ajoittaja
 
 import android.Manifest
-
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.ajoittaja.databinding.ActivityMainBinding
-
-private const val REQUEST_ENABLE_BT = 1
-private val REQUEST_BLUETOOTH_PERMISSION = 2
-private val PERMISSION_REQUEST_CODE = 200
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    private val REQUEST_ENABLE_BT = 1
-
-    // Register the permissions callback, which handles the user's response to the
-    // system permissions dialog. Save the return value, an instance of
-    // ActivityResultLauncher. You can use either a val, as shown in this snippet,
-    // or a lateinit var in your onAttach() or onCreate() method.
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // feature requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
-            }
+    // Bluetooth permission
+    private val REQUESTENABLEBT = 1
+    private val blRequestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Bluetooth permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show()
         }
-
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,36 +52,27 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
-        // Setup bluetooth
+        // Setup Bluetooth
         val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
-        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
         if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Closing the application", Toast.LENGTH_SHORT).show()
+            finish()
         }
 
-        requestBluetoothPermission()
-        // Enable Bluetooth if disabled
+        // Ensure that Bluetooth is enabled
         if (bluetoothAdapter?.isEnabled == false) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-        }
-
-
-    }
-
-    private fun requestBluetoothPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // Permission is already granted
-                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+            if (ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                blRequestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+                return
             }
-            else -> {
-                // Request the permission
-                requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
-            }
+            // TODO: Use ActivityResultContract
+            startActivityForResult(enableBtIntent, REQUESTENABLEBT)
         }
     }
 
@@ -122,7 +94,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
