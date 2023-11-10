fd5ad224e343b3bbb4b164b4521db594ce7d592e
Kasperi Kiviluoma <k.kiviluoma@pm.me>
2023-11-10 22:39:09 +0200

    test

--- app/src/main/java/com/example/ajoittaja/MainActivity.kt
+++ app/src/main/java/com/example/ajoittaja/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.example.ajoittaja
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.companion.AssociationInfo
+import android.companion.AssociationRequest
+import android.companion.BluetoothDeviceFilter
+import android.companion.CompanionDeviceManager
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.net.MacAddress
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.ajoittaja.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import java.util.concurrent.Executor
+import java.util.regex.Pattern
+
+private const val SELECT_DEVICE_REQUEST_CODE = 0
+private const val REQUEST_ENABLE_BT = 1
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+
+    // Bluetooth permission launcher
+    private val blRequestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Bluetooth permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val deviceManager: CompanionDeviceManager by lazy {
+        getSystemService(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager
+    }
+    val mBluetoothAdapter: BluetoothAdapter by lazy {
+        val java = BluetoothManager::class.java
+        getSystemService(java)!!.adapter
+    }
+    val executor: Executor = Executor { it.run() }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +76,92 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        // Setup Bluetooth
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Closing the application", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        // Ensure that Bluetooth is enabled
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                blRequestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+                return
+            }
+            // TODO: Use ActivityResultContract
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+
+        // To skip filters based on names and supported feature flags (UUIDs),
+        // omit calls to setNamePattern() and addServiceUuid()
+        // respectively, as shown in the following  Bluetooth example.
+        val deviceFilter: BluetoothDeviceFilter = BluetoothDeviceFilter.Builder()
+            .setNamePattern(Pattern.compile("Ajoittaja"))
+            //.addServiceUuid(ParcelUuid(UUID(0x123abcL, -1L)), null)
+            .build()
+
+        // The argument provided in setSingleDevice() determines whether a single
+        // device name or a list of them appears.
+        val pairingRequest: AssociationRequest = AssociationRequest.Builder()
+            .addDeviceFilter(deviceFilter)
+            .setSingleDevice(true)
+            .build()
+
+        // When the app tries to pair with a Bluetooth device, show the
+        // corresponding dialog box to the user.
+        deviceManager.associate(pairingRequest,
+            executor,
+            object : CompanionDeviceManager.Callback() {
+                // Called when a device is found. Launch the IntentSender so the user
+                // can select the device they want to pair with.
+                override fun onAssociationPending(intentSender: IntentSender) {
+                    intentSender.let {
+                        startIntentSenderForResult(it, SELECT_DEVICE_REQUEST_CODE, null, 0, 0, 0)
+                    }
+                }
+
+                override fun onAssociationCreated(associationInfo: AssociationInfo) {
+                    // AssociationInfo object is created and get association id and the
+                    // macAddress.
+                    var associationId: Int = associationInfo.id
+                    var macAddress: MacAddress? = associationInfo.deviceMacAddress
+                }
+
+                override fun onFailure(errorMessage: CharSequence?) {
+                    // Handle the failure.
+                }
+
+            }
+        )
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            SELECT_DEVICE_REQUEST_CODE -> when(resultCode) {
+                Activity.RESULT_OK -> {
+                    // The user chose to pair the app with a Bluetooth device.
+                    val deviceToPair: BluetoothDevice? =
+                        data?.getParcelableExtra(CompanionDeviceManager.EXTRA_DEVICE)
+                    deviceToPair?.let { device ->
+                        device.createBond()
+                        // Maintain continuous interaction with a paired device.
+                    }
+                }
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
